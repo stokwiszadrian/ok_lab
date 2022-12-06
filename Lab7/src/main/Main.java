2f4a01f7f02c934c4dbbbfe8ac3095ee1eceec51
Adrian Stokwisz <stokwiszadrian@gmail.com>
2022-12-06 19:15:54 +0100

    commented bigint, wrong calcs

--- Lab7/src/main/Main.java
+++ Lab7/src/main/Main.java
@@ -1,8 +1,17 @@
 package main;
 
 
+import karatsuba.Karatsuba;
+
+import java.math.BigInteger;
+
 public class Main {
 
     public static void main(String[] args) {
+        long newres = Karatsuba.karatsuba(12345, 6789);
+        long res = 12345 * 6789;
+        System.out.println("Normal multiplication: " + res);
+        System.out.println("Karatsuba multiplication: " + newres);
+        System.out.println("mine: " + Karatsuba.);
     }
 }
