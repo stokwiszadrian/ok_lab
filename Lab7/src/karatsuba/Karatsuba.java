6a7ae685d78c088dcf749a43401cc7c21961ce12
Adrian Stokwisz <stokwiszadrian@gmail.com>
2022-12-20 01:37:22 +0100

    lab7 poprawiony

--- Lab7/src/karatsuba/Karatsuba.java
+++ Lab7/src/karatsuba/Karatsuba.java
@@ -4,45 +4,27 @@ import java.math.BigInteger;
 
 public class Karatsuba {
 
-    public static String[] strCopy(long index, String string) {
-        StringBuilder first = new StringBuilder();
-        StringBuilder last = new StringBuilder();
-        long actualIndex = string.length() - index;
-        for (int i = 0; i<actualIndex; i++) {
-            first.append(string.charAt(i));
-        }
-        for (int i = (int)actualIndex; i<string.length(); i++) {
-            last.append(string.charAt(i));
-        }
-        return new String[] {first.toString(), last.toString()};
-    }
-    public static BigInteger calc(BigInteger num1, BigInteger num2) {
-        if (num1.compareTo(BigInteger.valueOf(10)) < 0 || num2.compareTo(BigInteger.valueOf(10)) < 0) {
-            return num1.multiply(num2);
-        }
+    public static BigInteger calc(BigInteger x, BigInteger y) {
 
-        String num1str = num1.toString();
-        String num2str = num2.toString();
+        // Użycie domyślnego mnożenia dla mniejszych liczb
+        int N = Math.max(x.bitLength(), y.bitLength());
+        if (N <= 20) return x.multiply(y);
 
-        int m = Math.max(num1str.length(), num2str.length()) ;
-        int m2 = m / 2;
-        System.out.println(num1 + "  " + num2);
-        System.out.println("DIVISIONS");
-        System.out.println(strCopy(m2, num1str)[0] + "  " + strCopy(m2, num1str)[1] + "  " + strCopy(m2, num2str)[0] + "  " + strCopy(m2, num2str)[1]);
-        BigInteger high1 = new BigInteger(strCopy(m2, num1str)[0]);
-        BigInteger low1 = new BigInteger(strCopy(m2, num1str)[1]);
-        BigInteger high2 = new BigInteger(strCopy(m2, num2str)[0]);
-        BigInteger low2 = new BigInteger(strCopy(m2, num2str)[1]);
+        // Liczba bitów podzielona na 2, zaokrąglona
+        N = (N / 2) + (N % 2);
 
+        // x = low1 + 2^N high1,   y = low2 + 2^N high2
+        BigInteger high1 = x.shiftRight(N);
+        BigInteger low1 = x.subtract(high1.shiftLeft(N));
+        BigInteger high2 = y.shiftRight(N);
+        BigInteger low2 = y.subtract(high2.shiftLeft(N));
 
-        BigInteger z0 = calc(low1, low2);
-        BigInteger z2 = calc(high1, high2);
-        BigInteger z1 = calc(low1.add(high1), low2.add(high2)).subtract(z2).subtract(z0);
+        // Obliczenia rekursywne
+        BigInteger ac    = calc(low1, low2);
+        BigInteger bd    = calc(high1, high2);
+        BigInteger abcd  = calc(low1.add(high1), low2.add(high2));
 
-        BigInteger z2multi = BigInteger.valueOf((long)Math.pow(10, 2 * m2));
-        BigInteger z1multi = BigInteger.valueOf((long) Math.pow(10, m2));
-
-        return ((z2.multiply(z2multi)).add(z1.multiply(z1multi))).add(z0);
+        return ac.add(abcd.subtract(ac).subtract(bd).shiftLeft(N)).add(bd.shiftLeft(2*N));
     }
 }
 
--- Lab7/src/main/Main.java
+++ Lab7/src/main/Main.java
@@ -6,17 +6,28 @@ import karatsuba.Karatsuba;
 import java.math.BigInteger;
 import java.util.Random;
 
+import static karatsuba.Karatsuba.calc;
+
 public class Main {
 
     public static void main(String[] args) {
-        Random rnd = new Random();
-        BigInteger n1 = new BigInteger(300, rnd);
-        BigInteger n2 = new BigInteger(300, rnd);
-        System.out.println(n1 + "  " + n2);
-        BigInteger res = n1.multiply(n2);
-        BigInteger karatsuba = Karatsuba.calc(n1, n2);
-        System.out.println("Normal multiplication: " + res);
-        System.out.println("Karatsuba multiplication: " + karatsuba);
-        System.out.println(res.equals(karatsuba));
+        long start, stop;
+        Random random = new Random();
+        int N = 5000;
+        BigInteger a = new BigInteger(N, random);
+        BigInteger b = new BigInteger(N, random);
+
+//        System.out.println(a + "    " + b);
+        start = System.currentTimeMillis();
+        BigInteger c = calc(a, b);
+        stop = System.currentTimeMillis();
+        System.out.println(stop - start);
+
+        start = System.currentTimeMillis();
+        BigInteger d = a.multiply(b);
+        stop = System.currentTimeMillis();
+        System.out.println(stop - start);
+
+        System.out.println((c.equals(d)));
     }
 }
